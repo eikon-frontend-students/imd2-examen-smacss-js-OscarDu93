0f53ebbee131f2ee59981c89d76b766d883e361f
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-02-05 17:15:21 +0100

    finish eval

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+var title = document.querySelector(".branding-title");
+var one = document.querySelector(".article");
+var two = document.querySelector(".onions");
+var three = document.querySelector(".herby");
+var four = document.querySelector(".smooth");
+
+function colorChange() {
+  title.classList.add("is-active");
+  one.classList.add(".onions");
+}
+
+button.addEventListener("click", colorChange);
